261becacef3d35e22580718274411da878387541
Bima Sanjaya <122960409+Bimasanjaya0704@users.noreply.github.com>
2024-07-16 08:21:46 +0700

    Update value 0 = 0

--- Program.cs
+++ Program.cs
@@ -1,58 +1,59 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using System.Text;
 
 public class FooBar
 {
-    public static bool Foo(int n)
-    {
-        return n % 3 == 0;
-    }
+	public static bool Foo(int n)
+	{
+		return n % 3 == 0;
+	}
 
-    public static bool Bar(int n)
-    {
-        return n % 5 == 0;
-    }
+	public static bool Bar(int n)
+	{
+		return n % 5 == 0;
+	}
 
-    public static void FooAndBar(int n)
-    {
-        StringBuilder s = new();
-        for (int i = 1; i <= n; i++)
-        {
-            bool isShow = true;
-            if (Foo(i) && Bar(i))
-            {
-                s.Append("Foobar");
-                isShow = false;
-            }
-            else if (Foo(i))
-            {
-                s.Append("Foo");
-                isShow = false;
-            }
-            else if (Bar(i))
-            {
-                s.Append("Bar");
-                isShow = false;
-            }
-            if (isShow)
-            {
-                s.AppendLine(i.ToString());
-            }
-            else
-            {
-                s.AppendLine();
-            }
+	public static void FooAndBar(int n)
+	{
+		StringBuilder s = new();
+		for (int i = 0; i <= n; i++)
+		{
+			bool isShow = true;
+			if (i != 0 & Foo(i) && Bar(i))
+			{
+				s.Append("Foobar");
+				isShow = false;
+			}
+			else if (i != 0 && Foo(i))
+			{
+				s.Append("Foo");
+				isShow = false;
+			}
+			else if (i != 0 && Bar(i))
+			{
+				s.Append("Bar");
+				isShow = false;
+			}
+			if (isShow)
+			{
+				s.AppendLine(i.ToString());
+			}
+			else
+			{
+				s.AppendLine();
+			}
 
-        }
-        Console.WriteLine(s.ToString());
-    }
-    static void Main()
-    {
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
-        int n = 15;
-        FooAndBar(n);
-        stopwatch.Stop();
-        Console.WriteLine($"Time : {stopwatch.ElapsedMilliseconds} ms");
-    }
+		}
+		Console.WriteLine(s.ToString());
+	}
+
+	static void Main()
+	{
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.Start();
+		int n = 15;
+		FooAndBar(n);
+		stopwatch.Stop();
+		Console.WriteLine($"Time : {stopwatch.ElapsedMilliseconds} ms");
+	}
 }
